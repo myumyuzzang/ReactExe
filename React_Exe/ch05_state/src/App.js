a739a4bd6fe9caaa77e1dda045757486cb61f5c4
myumyuzzang <myu.myu.zzang@gmail.com>
2024-05-20 10:50:45 +0900

    2024 05 20 수업 내용 추가

---
+++ React_Exe/ch05_state/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+// import Counter from './ex01_class_state/Counter';
+// import Counter from './ex02_class_multistate/Counter';
+// import Counter from './ex03_class_continuous_call/Counter';
+// import Counter from './ex04_class_continuous_call/Counter';
+import Counter from './ex05_class_later_state/Counter';
+
+function App() {
+  return <Counter/>
+
+}
+
+export default App;
