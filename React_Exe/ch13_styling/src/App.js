1fc4cd651936e234cea1471c57ac1c60fa05e5ac
myumyuzzang <myu.myu.zzang@gmail.com>
2024-05-23 12:55:45 +0900

    2024 05 23 sass 수업

---
+++ React_Exe/ch13_styling/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import React, {Component} from 'react';
+import SassComponent from './ex01_sass/SassComponent';
+
+class App extends Component {
+  render(){
+    return (
+      <div>
+        <SassComponent/>
+      </div>
+    )
+  }
+
+}
+
+export default App;
