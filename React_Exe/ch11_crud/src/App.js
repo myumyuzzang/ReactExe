ec3c0d9ba3218ea74c343c87fde73a071fcd4c3f
myumyuzzang <myu.myu.zzang@gmail.com>
2024-05-23 10:57:45 +0900

    2024 05 22 수업 내용 추가

--- React_Exe/ch11_crud/src/App.js
+++ React_Exe/ch11_crud/src/App.js
@@ -2,8 +2,11 @@ import logo from "./logo.svg";
 import "./App.css";
 
 // import SubApp from "./ex30_update/SubApp";
-import SubApp from "./ex31_update/SubApp";
-
+// import SubApp from "./ex31_update/SubApp";
+// import SubApp from "./ex32_delete/SubApp";
+// import SubApp from "./ex33_delete/SubApp";
+// import SubApp from "./ex34_delete/SubApp";
+import SubApp from "./ex35_divide_file/SubApp";
 
 
 
