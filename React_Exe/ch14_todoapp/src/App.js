85986fc2a056ffb958ec4dee68875fa358f9d0d7
myumyuzzang <myu.myu.zzang@gmail.com>
2024-05-24 14:34:02 +0900

    2024 05 24 수업 내용 추가

--- React_Exe/ch14_todoapp/src/App.js
+++ React_Exe/ch14_todoapp/src/App.js
@@ -1,14 +1,49 @@
 import logo from './logo.svg';
 import './App.css';
-// import TodoTemplate from './ex01_TodoTemplate/TodoTemplate';
-import TodoTemplate from './ex02_TodoInsert/TodoTemplate';
-import TodoInsert from './ex02_TodoInsert/TodoInsert';
+import { useState, useRef, useCallback } from 'react';
+import TodoTemplate from "./ex05_TodoInsert/TodoTemplate";
+import TodoInsert from "./ex05_TodoInsert/TodoInsert";
+import TodoList from "./ex05_TodoInsert/TodoList";
 
 
 function App() {
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: "리액트의 기초 알아보기",
+      checked: true,
+    },
+    {
+      id: 2,
+      text: "컴포턴트 스타일링 해보기",
+      checked: true,
+    },
+    {
+      id: 3,
+      text: "일정 관리 앱 만들어보기",
+      checked: false,
+    },            
+  ]);
+  
+  const nextId = useRef(4);
+
+  const onInsert = useCallback(
+    text => {
+      const todo = {
+        id: nextId.current,
+        text,
+        checked: false
+      }
+      setTodos(todos.concat(todo));
+      nextId.current += 1;
+    },
+    [todos]
+  );
+  
   return (
     <TodoTemplate>
-      <TodoInsert></TodoInsert>
+      <TodoInsert onInsert={onInsert}/>
+      <TodoList todos={todos}/>
     </TodoTemplate>
     );
   
---
+++ React_Exe/ch14_todoapp/src/ex05_TodoInsert/TodoInsert.jsx
@@ -0,0 +1,38 @@
+import {MdAdd, Md9K} from 'react-icons/md';
+import './TodoInsert.scss';
+import { useState } from 'react';
+import { useCallback } from 'react';
+
+const TodoInsert = ({onInsert}) => {
+
+    const [value, setValue] = useState('');
+
+    const onChange = useCallback( e=> {
+        setValue(e.target.value);
+    }, []);
+
+    const onSubmit = useCallback(
+        e => {
+            onInsert(value);
+            setValue('');
+
+            e.preventDefault();
+        },
+        [onInsert, value]
+    );
+
+    return (
+        <form className='TodoInsert' onSubmit={onSubmit}>
+            <input placeholder='할 일을 입력하세요' 
+                    value={value}
+                    onChange={onChange}
+            />
+            <button type='submit'>
+                <MdAdd/>
+                {/* <Md9K /> */}
+            </button>
+        </form>
+    )
+}
+
+export default TodoInsert;
