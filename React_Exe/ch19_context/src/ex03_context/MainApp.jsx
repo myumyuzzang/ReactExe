880598187bd109f886be30dcfe371a6251b38ef0
myumyuzzang <myu.myu.zzang@gmail.com>
2024-05-30 12:31:50 +0900

    2024 05 30 수업 내용 추가

---
+++ React_Exe/ch19_context/src/ex03_context/MainApp.jsx
@@ -0,0 +1,14 @@
+import './style.css';
+import Sub1 from './Sub1';
+
+
+function MainApp(){
+    return (
+        <div className='root'>
+            <h1>Hello World</h1>
+            <Sub1/>
+        </div>
+    )
+}
+
+export default MainApp;
