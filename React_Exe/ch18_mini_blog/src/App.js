0a0e4acbef50f795f3e45711591960e424a6dbdf
myumyuzzang <myu.myu.zzang@gmail.com>
2024-05-30 11:12:26 +0900

    2024 05 30 미니블로그 app 완성

--- React_Exe/ch18_mini_blog/src/App.js
+++ React_Exe/ch18_mini_blog/src/App.js
@@ -1,6 +1,6 @@
 import logo from "./logo.svg";
 import "./App.css";
-import MiniBlogApp from "./ex06_post_view_page/MiniBlogApp";
+import MiniBlogApp from "./ex07_comment_list/MiniBlogApp";
 
 function App() {
   return <MiniBlogApp />;
